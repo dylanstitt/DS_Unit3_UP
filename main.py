f0a2e80133f1e8e5323db25deac054e7461b93e2
Dylan Stitt <dstitt527@gmail.com>
2024-11-18 12:40:35 -0500

    init

---
+++ main.py
@@ -0,0 +1,31 @@
+# Dylan Stitt
+# Unit 3 Unit Project
+# Turtle Tree
+
+import turtle
+
+
+def draw(t, color, thickness, angle):
+    ...
+
+
+def changeColor(rgb):
+    ...
+
+
+def chanceThickness(thickness):
+    ...
+
+
+def chanceRotation(angle):
+    ...
+
+
+def main():
+    t = turtle.Turtle()
+
+    turtle.mainloop()
+
+
+if __name__ == '__main__':
+    main()
