085f59279a68da9919a11cd54895df7ac67e7800
Dylan Stitt <dstitt527@gmail.com>
2024-11-18 14:49:56 -0500

    mid

--- main.py
+++ main.py
@@ -1,28 +1,52 @@
 # Dylan Stitt
 # Unit 3 Unit Project
-# Turtle Tree
+# Recursive Tree
 
-import turtle
+import turtle, random
 
+######################################### GREEN LEAVES AND RANDOM CHILDREN OFF BRANCHES
 
-def draw(t, color, thickness, angle):
-    ...
+DA = 2
+DL = 5
+DT = .5
 
+def draw(t, treeLength, rotationAngle, branchLength, thickness):
+    if treeLength == 1:
+        return
 
-def changeColor(rgb):
-    ...
+    t.pensize(thickness)
+    num = random.randint(0, 2)
+    rotNum = random.randint(-7, 7)
 
+    if num != 0:
+        t.forward(branchLength)
+        t.left(rotationAngle)
+        draw(t, treeLength - 1, rotationAngle + rotNum, branchLength - DL, thickness - DT)
 
-def chanceThickness(thickness):
-    ...
+        if num == 2:
+            t.right(2*rotationAngle)
+            draw(t, treeLength - 1, rotationAngle+rotNum, branchLength-DL, thickness-DT)
+            t.left(rotationAngle)
+            t.backward(branchLength)
+            return
+    return
 
+def setup(t):
+    turtle.mode("logo")
+    t.color("brown4")
+    t.speed("fastest")
+    t.hideturtle()
 
-def chanceRotation(angle):
-    ...
-
+    t.penup()
+    t.setx(0)
+    t.sety(-250)
+    t.pendown()
 
 def main():
     t = turtle.Turtle()
+
+    setup(t)
+    draw(t, 8, 15, 80, 4)
 
     turtle.mainloop()
 
